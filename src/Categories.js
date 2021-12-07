708c587e7d17823a5f9b88778140c25e7530cd50
shakhzod1999 <shahzodtoyirov99@gmail.com>
2021-12-08 00:53:49 +0500

    foodMenuApp

---
+++ src/Categories.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Categories = ({ filterItems }) => {
+    return (
+        <div className="btn-container">
+            <button className="filter-btn" onClick={() => filterItems("all")}>
+                All
+            </button>
+            <button
+                className="filter-btn"
+                onClick={() => filterItems("breakfast")}
+            >
+                Breakfast
+            </button>
+            <button className="filter-btn" onClick={() => filterItems("lunch")}>
+                Lunch
+            </button>
+            <button
+                className="filter-btn"
+                onClick={() => filterItems("shakes")}
+            >
+                Shakes
+            </button>
+        </div>
+    );
+};
+
+export default Categories;
